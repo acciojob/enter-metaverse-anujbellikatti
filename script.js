1e2773ae13572ffec615787a0a86ec8231867030
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-19 13:59:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-//your JS code here. If required.
+// //your JS code here. If required.
 
-function hi(){
-let x = document.querySelector("#status");
-	let s =  document.createElement("h1")
-	s.innerHTML = "Entered Metaverse";
-	s.id = "status"
-	x.replaceWith(s);
-	let a = "have.descendants', 'h1"
-	// document.body.appendChild(s);
-	// document.body.removeChild(x);
-}
+// function hi(){
+// let x = document.querySelector("#status");
+// 	let s =  document.createElement("h1")
+// 	s.innerHTML = "Entered Metaverse";
+// 	s.id = "status"
+// 	x.replaceWith(s);
+// 	let a = "have.descendants', 'h1"
+// 	// document.body.appendChild(s);
+// 	// document.body.removeChild(x);
+// }
+
+const statusParagraph = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+
+enterButton.addEventListener("click", function () {
+    // Create a new h1 element
+    const newHeading = document.createElement("h1");
+    newHeading.textContent = "Entered Metaverse";
+    statusParagraph.replaceWith(newHeading);
+});
