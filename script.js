0cf1c131271a3dcb8bf8eb431a242a47278101a2
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-19 13:41:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+function hi(){
+let x = document.getElementById("status");
+	let s =  document.createElement("h1")
+	s.innerHTML = x.innerHTML;
+	x.replaceWith(s);
+	// document.body.appendChild(s);
+	// document.body.removeChild(x);
+}
