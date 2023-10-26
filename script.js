405055cfe2851fdb8f1c9d9bedb37ce80fad8b92
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-26 21:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 // //your JS code here. If required.
 
-function hi(){
-let x = document.querySelector("#status");
-	
-	const enterButton = document.getElementById("enterBtn");
-	enterButton.innerHTML = x.innerHTML;
-	
-	let s =  document.createElement("h1")
-	s.innerHTML = "Entered Metaverse";
-	s.id = "status"
-	x.replaceWith(s);
-	x.remove();
-	// document.body.appendChild(s);
-	// document.body.removeChild(x);
-}
 
-// document.addEventListener("DOMContentLoaded", function () {
-//     const statusElement =document.querySelector("#status");
-//     const enterButton = document.getElementById("enterBtn");
+// function hi(){
+// let x = document.querySelector("#status");
+	
+// 	const enterButton = document.getElementById("enterBtn");
+// 	enterButton.innerHTML = x.innerHTML;
+	
+// 	let s =  document.createElement("h1")
+// 	s.innerHTML = "Entered Metaverse";
+// 	s.id = "status"
+// 	x.replaceWith(s);
+// 	x.remove();
+// 	// document.body.appendChild(s);
+// 	// document.body.removeChild(x);
+// }
+const btn=document.getElementById("enterBtn");
+const pa=document.getElementById("status");
 
-//     enterButton.addEventListener("click", function () {
-//         statusElement.innerHTML = '<hi>Entered Metaverse</hi>';
-//     });
-// });
+btn.addEventListener("click", function(){
+	let h1=document.createElement("h1");
+	h1.id="status";
+	h1.innerText="Entered Metaverse";
+	document.body.insertBefore(h1,pa);
+	pa.remove();
+});
