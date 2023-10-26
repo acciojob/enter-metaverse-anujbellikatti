39dbd3498eba33408112e4da1311da709551fcd3
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-26 21:07:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // //your JS code here. If required.
 
 function hi(){
-	const enterButton = document.getElementById("enterBtn");
-	
 let x = document.querySelector("#status");
+	
+	const enterButton = document.getElementById("enterBtn");
+	enterButton.innerHTML = x.innerHTML;
+	
 	let s =  document.createElement("h1")
 	s.innerHTML = "Entered Metaverse";
 	s.id = "status"
 	x.replaceWith(s);
 	x.remove();
-	enterButton.innerHTML = s.id;
 	// document.body.appendChild(s);
 	// document.body.removeChild(x);
 }
