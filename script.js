2facb15c52ab64a7a7dd1bb4da4e6786ab491b40
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-26 21:14:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,17 +12,7 @@ let x = document.querySelector("#status");
 	s.id = "status"
 	x.replaceWith(s);
 	x.remove();
-	document.body.insertBefore(enterButton);
+	
 	// document.body.appendChild(s);
 	// document.body.removeChild(x);
 }
-// const btn=document.getElementById("enterBtn");
-// const pa=document.getElementById("status");
-
-// btn.addEventListener("click", function(){
-// 	let h1=document.createElement("h1");
-// 	h1.id="status";
-// 	h1.innerText="Entered Metaverse";
-// 	document.body.insertBefore(h1,pa);
-// 	pa.remove();
-// });
